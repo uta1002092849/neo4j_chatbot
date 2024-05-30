297302482100800e917740ee7bea9deec8bede9f
uta1002092849 <mmt2849@mavs.uta.edu>
2024-05-30 15:15:22 -0500

    remove utiles

--- utils.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def write_message(role, content, save = True):
-    if save:
-        st.session_state.messages.append({"role": role, "content": content})
-
-    with st.chat_message(role):
-        st.markdown(content)
