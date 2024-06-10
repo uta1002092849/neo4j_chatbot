0ef8a3a4e5c174d9c7c88f5005b0009a75ddb266
uta1002092849 <mmt2849@mavs.uta.edu>
2024-06-10 13:42:47 -0500

    add try catch to catch error connection

--- graph.py
+++ graph.py
@@ -1,8 +1,12 @@
 import streamlit as st
 from langchain_community.graphs import Neo4jGraph
 
-graph = Neo4jGraph(
-    url = st.secrets['NEO4J_URI'],
-    username = st.secrets['NEO4J_USERNAME'],
-    password = st.secrets['NEO4J_PASSWORD']
-)
+try:
+    graph = Neo4jGraph(
+        url = st.secrets['NEO4J_URI'],
+        username = st.secrets['NEO4J_USERNAME'],
+        password = st.secrets['NEO4J_PASSWORD']
+    )
+except Exception as e:
+    st.error(f"Error connecting to the database with the error message:\n {e}")
+    st.stop()
