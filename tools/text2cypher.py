9fcdf2f35f825437c95f3f7fe5a52fcf628380f6
uta1002092849 <mmt2849@mavs.uta.edu>
2024-06-12 16:35:22 -0500

    add attempt feature to give the model more chances to fix it error

--- tools/text2cypher.py
+++ tools/text2cypher.py
@@ -43,16 +43,19 @@ chain = GraphCypherQAChain.from_llm(
 )
 
 def generate_cypher(prompt_text):
-    try:
-        response = chain.invoke({"query":prompt_text})
-    except Exception as e:
-        response = {"result": "Sorry, I can only answer questions related to the sockg dataset"}
+    attempt = 3
+    while attempt > 0:
+        try:
+            response = chain.invoke({"query":prompt_text})
+            break
+        except Exception as e:
+            attempt -= 1
+            if attempt == 0:
+                raise Exception("Sorry, I could not process your request. Please try to rephrase your question or try again later.")
     
     # check if intermediate steps are present in response
     if 'intermediate_steps' not in response:
         raise Exception("Sorry, I could not process your request. Please try to rephrase your question or try again later.")
     
     constructed_cypher = response['intermediate_steps'][0]['query']
-    constructed_context = response['intermediate_steps'][1]['context']
-    final_response = response['result']
-    return {"constructed_cypher": constructed_cypher, "constructed_context": constructed_context, "final_response": final_response}
+    return {"constructed_cypher": constructed_cypher}
