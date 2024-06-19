ee0a2bb3b0a3cae57b7aabceb27bb25b4acae5a2
Huy <maitran.mt02@gmail.com>
2024-06-19 13:56:19 -0500

    remove config window when deploy

--- streamlit_app/dashboard.py
+++ streamlit_app/dashboard.py
@@ -6,12 +6,25 @@ from models.llms import gemini_pro
 from components.sidebar_chatbot import sidebar_chatbot
 from components.field_tab import field_tab_component
 from components.expUnit_tab import expUnit_tab_component
-from components.treatment_tab import treatment_tab_component
 from components.text2cypher_tab import text2cypher_component
 
-driver = init_driver()
 # Page config and icon
 st.set_page_config(layout="wide", page_title="SOCKG Chat Bot", page_icon=":evergreen_tree:")
+
+# hide hamburger menu, deploy button, and footer
+st.markdown("""
+    <style>
+        .reportview-container {
+            margin-top: -2em;
+        }
+        #MainMenu {visibility: hidden;}
+        .stDeployButton {display:none;}
+        footer {visibility: hidden;}
+        #stDecoration {display:none;}
+    </style>
+""", unsafe_allow_html=True)
+
+driver = init_driver()
 
 
 # Initialize bot messages in session state
