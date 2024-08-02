0d54737a3930cffbf831e4ac08dba8d88f6e2f0c
Huy <maitran.mt02@gmail.com>
2024-08-01 22:46:19 -0500

    Group pages into relevant group

--- streamlit_app/components/navigation_bar.py
+++ streamlit_app/components/navigation_bar.py
@@ -3,9 +3,15 @@ import streamlit as st
 def navition_bar():
     st.sidebar.title("Navigation")
     with st.sidebar:
-        st.page_link("dashboard.py", label="Home", icon="🏡")
-        st.page_link("pages/_Fields.py", label="Field Explorer", icon="🏞️")
-        st.page_link("pages/_ExperimentalUnits.py", label="Experimental Unit Explorer", icon="📐")
-        st.page_link("pages/_Treatments.py", label="Treatment Explorer", icon="💊")
-        st.page_link("pages/_WeatherStations.py", label="Weather Station Explorer", icon="🌡️")
-        st.page_link("pages/_Text2Cypher.py", label="Text2Cypher", icon="⌨️")
+        # Group pages into home, explorers, and text2cypher
+        with st.expander("Home Pages", expanded=True):
+            st.page_link("dashboard.py", label="Home", icon="🏡")
+        
+        with st.expander("Explorers", expanded=True):
+            st.page_link("pages/_Fields.py", label="Field Explorer", icon="🏞️")
+            st.page_link("pages/_ExperimentalUnits.py", label="Experimental Unit Explorer", icon="📐")
+            st.page_link("pages/_Treatments.py", label="Treatment Explorer", icon="💊")
+            st.page_link("pages/_WeatherStations.py", label="Weather Station Explorer", icon="🌡️")
+        
+        with st.expander("Natural Language Decode", expanded=True):
+            st.page_link("pages/_Text2Cypher.py", label="Text2Cypher", icon="⌨️")
