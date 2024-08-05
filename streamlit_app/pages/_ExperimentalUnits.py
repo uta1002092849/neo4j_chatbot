23cf306c5fdb280c237c034800afe59e6560f978
uta1002092849 <mmt2849@mavs.uta.edu>
2024-08-05 14:12:08 -0500

    Add data element next to chart

--- streamlit_app/pages/_ExperimentalUnits.py
+++ streamlit_app/pages/_ExperimentalUnits.py
@@ -77,125 +77,103 @@ if selected_row:
     st.session_state.selected_treatment = treatments_df.loc[selected_row[0], "ID"]
     st.switch_page("pages/_Treatments.py")
 
-# Display grain yield and soil carbon storage of an experimental unit over time
-# get grain yield of an experimental unit over time
-grain_yield_df = exp_unit_dao.get_grain_yield(st.session_state.selected_exp_unit)
 st.subheader("Grain Yield Over Time")
-# check for empty dataframe
+grain_yield_df = exp_unit_dao.get_grain_yield(st.session_state.selected_exp_unit)
 if grain_yield_df is not None and not grain_yield_df.empty:
-    # remove any row that contains a null value in either column
     grain_yield_df = grain_yield_df.dropna()
-
-    # Convert the date column to datetime
     grain_yield_df['Date'] = pd.to_datetime(grain_yield_df['Date'])
-    # Ground grain yield to 2 decimal places
     grain_yield_df['grainYield'] = grain_yield_df['grainYield'].astype(float).round(2)
-    # rename columns for better display
-    grain_yield_df.rename(columns={
-        "Date": "Date",
-        "grainYield": "Grain Yield"
-    }, inplace=True)
-    st.line_chart(grain_yield_df, x='Date', y='Grain Yield')
+    grain_yield_df.rename(columns={"Date": "Date", "grainYield": "Grain Yield"}, inplace=True)
+    
+    tab1, tab2 = st.tabs(["Chart", "Data"])
+    with tab1:
+        st.line_chart(grain_yield_df, x='Date', y='Grain Yield')
+    with tab2:
+        avg_yield = grain_yield_df['Grain Yield'].mean()
+        min_yield = grain_yield_df['Grain Yield'].min()
+        max_yield = grain_yield_df['Grain Yield'].max()
+        
+        cols = st.columns(3)
+        with cols[0]:
+            st.metric("Average Yield", f"{avg_yield:.2f}")
+        with cols[1]:
+            st.metric("Minimum Yield", f"{min_yield:.2f}")
+        with cols[2]:
+            st.metric("Maximum Yield", f"{max_yield:.2f}")
+        
+        st.dataframe(grain_yield_df.style.highlight_max(axis=0), use_container_width=True, hide_index=True)
 else:
     st.write("No grain yield data available for this Experimental Unit.")
 
-
-# get soil carbon storage of an experimental unit over time
+st.subheader("Soil Carbon Storage Over Time")
 soil_carbon_df = exp_unit_dao.get_soil_carbon(st.session_state.selected_exp_unit)
-
-# Remove empty row if its soil carbon is None
 soil_carbon_df = soil_carbon_df[soil_carbon_df['SoilCarbon'].notnull()]
-# rename columns for better display
 soil_carbon_df.rename(columns={
-    "Date": "Date",
-    "SoilCarbon": "Soil Carbon",
-    "LowerDepth": "Lower Depth",
-    "UpperDepth": "Upper Depth"
+    "Date": "Date", "SoilCarbon": "Soil Carbon",
+    "LowerDepth": "Lower Depth", "UpperDepth": "Upper Depth"
 }, inplace=True)
 
-
-st.subheader("Soil Carbon Storage Over Time")
-# check for empty dataframe
 if soil_carbon_df is not None and not soil_carbon_df.empty:
-
-    # Create a new column for the depth, which is the average of the upper and lower depth
     soil_carbon_df['Average Depth'] = (soil_carbon_df['Lower Depth'] + soil_carbon_df['Upper Depth']) // 2
-
-    # Plot the 3D soil carbon storage graph, where the x-axis is the date, y-axis is the average depth, and z-axis is the soil carbon
-    figure = px.scatter_3d(soil_carbon_df, x='Date', y='Average Depth', z='Soil Carbon', color='Soil Carbon')
-    st.plotly_chart(
-        figure_or_data=figure,
-        use_container_width=True
-    )
+    
+    tab1, tab2 = st.tabs(["Chart", "Data"])
+    with tab1:
+        figure = px.scatter_3d(soil_carbon_df, x='Date', y='Average Depth', z='Soil Carbon', color='Soil Carbon')
+        st.plotly_chart(figure, use_container_width=True)
+    with tab2:
+        avg_carbon = soil_carbon_df['Soil Carbon'].mean()
+        min_carbon = soil_carbon_df['Soil Carbon'].min()
+        max_carbon = soil_carbon_df['Soil Carbon'].max()
+        cols = st.columns(3)
+        with cols[0]:
+            st.metric("Average Soil Carbon", f"{avg_carbon:.2f}")
+        with cols[1]:
+            st.metric("Minimum Soil Carbon", f"{min_carbon:.2f}")
+        with cols[2]:
+            st.metric("Maximum Soil Carbon", f"{max_carbon:.2f}")
+        
+        st.dataframe(soil_carbon_df.style.highlight_max(axis=0), use_container_width=True, hide_index=True)
 else:
     st.write("No soil carbon data available for this Experimental Unit.")
-    
 
-# get soil chemical properties of an experimental unit over time
-soil_chemical_df = exp_unit_dao.get_soil_chemical_properties(st.session_state.selected_exp_unit)
 st.subheader("Other Soil Chemical Properties Over Time")
-# check for empty dataframe
+soil_chemical_df = exp_unit_dao.get_soil_chemical_properties(st.session_state.selected_exp_unit)
 if soil_chemical_df is not None and not soil_chemical_df.empty:
-    # remove any row that contains a null value in Date 
-    soil_carbon_df = soil_carbon_df[soil_carbon_df['Date'].notnull()]
-
-    # Convert the date column to datetime
+    soil_chemical_df = soil_chemical_df[soil_chemical_df['Date'].notnull()]
     soil_chemical_df['Date'] = pd.to_datetime(soil_chemical_df['Date'])
-
-    # Create a new column for the depth, which is the average of the upper and lower depth
     soil_chemical_df['Average Depth'] = (soil_chemical_df['LowerDepth'] + soil_chemical_df['UpperDepth']) // 2
-
-    # Rename columns for better display
     soil_chemical_df.rename(columns={
-        "Date": "Date",
-        "Ammonium": "Ammonium",
-        "Nitrate": "Nitrate",
-        "PH": "pH",
-        "Nitrogen": "Total Nitrogen",
+        "Date": "Date", "Ammonium": "Ammonium", "Nitrate": "Nitrate",
+        "PH": "pH", "Nitrogen": "Total Nitrogen",
     }, inplace=True)
 
-    # 4 tabs for each soil chemical property
-    tabs = st.tabs(["Ammonium", "Nitrate", "pH", "Total Nitrogen"])
-    with tabs[0]:
-        # check if all values in the Ammonium column are null
-        if soil_chemical_df['Ammonium'].isnull().all():
-            st.write("No Ammonium data available for this Experimental Unit.")
-            
-        else:
-            figure = px.scatter_3d(soil_chemical_df, x='Date', y='Average Depth', z='Ammonium', color='Ammonium')
-            st.plotly_chart(
-                figure_or_data=figure,
-                use_container_width=True
-            )
-    with tabs[1]:
-        # check if all values in the Nitrate column are null
-        if soil_chemical_df['Nitrate'].isnull().all():
-            st.write("No Nitrate data available for this Experimental Unit.")
-        else:
-            figure = px.scatter_3d(soil_chemical_df, x='Date', y='Average Depth', z='Nitrate', color='Nitrate')
-            st.plotly_chart(
-                figure_or_data=figure,
-                use_container_width=True
-            )
-    with tabs[2]:
-        # check if all values in the pH column are null
-        if soil_chemical_df['pH'].isnull().all():
-            st.write("No pH data available for this Experimental Unit.")
-        else:
-            figure = px.scatter_3d(soil_chemical_df, x='Date', y='Average Depth', z='pH', color='pH')
-            st.plotly_chart(
-                figure_or_data=figure,
-                use_container_width=True
-            )
-    with tabs[3]:
-        # check if all values in the Total Nitrogen column are null
-        if soil_chemical_df['Total Nitrogen'].isnull().all():
-            st.write("No Total Nitrogen data available for this Experimental Unit.")
-        else:
-            figure = px.scatter_3d(soil_chemical_df, x='Date', y='Average Depth', z='Total Nitrogen', color='Total Nitrogen')
-            st.plotly_chart(
-                figure_or_data=figure,
-                use_container_width=True
-            )
+    col1, col2 = st.columns(2)
+    properties = ["Ammonium", "Nitrate", "pH", "Total Nitrogen"]
+    
+    for i, prop in enumerate(properties):
+        with col1 if i % 2 == 0 else col2:
+            st.subheader(prop)
+            if soil_chemical_df[prop].isnull().all():
+                st.write(f"No {prop} data available for this Experimental Unit.")
+            else:
+                tab1, tab2 = st.tabs(["Chart", "Data"])
+                with tab1:
+                    figure = px.scatter_3d(soil_chemical_df, x='Date', y='Average Depth', z=prop, color=prop)
+                    st.plotly_chart(figure, use_container_width=True)
+                with tab2:
+                    avg_value = soil_chemical_df[prop].mean()
+                    min_value = soil_chemical_df[prop].min()
+                    max_value = soil_chemical_df[prop].max()
+
+                    cols = st.columns(3)
+                    
+                    with cols[0]:
+                        st.metric(f"Average {prop}", f"{avg_value:.2f}")
+                    with cols[1]:
+                        st.metric(f"Minimum {prop}", f"{min_value:.2f}")
+                    with cols[2]:
+                        st.metric(f"Maximum {prop}", f"{max_value:.2f}")
+                    
+                    st.dataframe(soil_chemical_df[['Date', 'Average Depth', prop]].style.highlight_max(axis=0), use_container_width=True, hide_index=True)
 else:
     st.write("No soil chemical data available for this Experimental Unit.")
