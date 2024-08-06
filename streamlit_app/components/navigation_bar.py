710b6a4453104e5b595eed97378faf1630811816
uta1002092849 <mmt2849@mavs.uta.edu>
2024-08-06 16:46:16 -0500

    add feedback page

--- streamlit_app/components/navigation_bar.py
+++ streamlit_app/components/navigation_bar.py
@@ -15,3 +15,6 @@ def navition_bar():
         
         with st.expander("Natural Language Decode", expanded=True):
             st.page_link("pages/_Text2Cypher.py", label="Text2Cypher", icon="⌨️")
+        
+        with st.expander("Feedback", expanded=True):
+            st.page_link("pages/_Feedback.py", label="Feedback", icon="📝")
